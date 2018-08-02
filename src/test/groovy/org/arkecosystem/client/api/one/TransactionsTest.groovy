81d91985bc539175ad001c58546b5ef508e90a34
Brian Faust <envoyer@pm.me>
2018-08-02 10:08:20 +0300

    test: v1 transaction dummy
    Signed-off-by: Brian Faust <envoyer@pm.me>

---
+++ src/test/groovy/org/arkecosystem/client/api/one/TransactionsTest.groovy
@@ -0,0 +1,15 @@
+package org.arkecosystem.client.api.one
+
+import spock.lang.Specification
+import org.arkecosystem.client.Connection
+
+class TransactionsTest extends Specification {
+    def "all"() {
+        setup:
+            def connection = new Connection('https://explorer.ark.io:8443/api/')
+        when:
+            def actual = connection.transactions().all()
+        then:
+            actual.success == true
+    }
+}
