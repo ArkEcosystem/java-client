583399209a250e212a3836c48e3125daf7bcd0a6
supaiku <supaiku@protonmail.com>
2018-10-06 23:36:46 +0200

    port: ConnectionManagerTest

---
+++ src/test/java/org/arkecosystem/client/ConnectionManagerTest.java
@@ -0,0 +1,85 @@
+package org.arkecosystem.client;
+
+import org.arkecosystem.client.api.one.One;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConnectionManagerTest {
+
+    @Test
+    public void connect() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("host", "dummy");
+        map.put("version", 1);
+
+        ConnectionManager manager = new ConnectionManager();
+        manager.connect(map);
+        assertEquals(1, manager.getConnections().size());
+    }
+
+    @Test
+    public void disconnect() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("host", "dummy");
+        map.put("version", 1);
+
+        ConnectionManager manager = new ConnectionManager();
+        manager.connect(map);
+        assertEquals(1, manager.getConnections().size());
+        manager.disconnect();
+        assertEquals(0, manager.getConnections().size());
+    }
+
+    public void connection() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("host", "dummy");
+        map.put("version", 1);
+
+        ConnectionManager manager = new ConnectionManager();
+        manager.connect(map);
+        Connection<One> connection = manager.connection("main");
+        assertNotNull(connection);
+        assertEquals(One.class, connection.api().getClass());
+    }
+
+    @Test
+    public void getDefaultConnection() {
+        ConnectionManager manager = new ConnectionManager();
+        String actual = manager.getDefaultConnection();
+        assertEquals("main", actual);
+    }
+
+    @Test
+    public void setDefaultConnection() {
+        ConnectionManager manager = new ConnectionManager();
+        assertEquals("main", manager.getDefaultConnection());
+        manager.setDefaultConnection("dummy");
+        assertEquals("dummy", manager.getDefaultConnection());
+    }
+
+    @Test
+    public void getConnections() {
+        ConnectionManager manager = new ConnectionManager();
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("host", "dummy");
+        map.put("version", 1);
+
+        manager.connect(map);
+
+        map = new HashMap<>();
+        map.put("host", "dummy");
+        map.put("version", 2);
+
+        manager.connect(map, "backup");
+
+        assertEquals(2, manager.getConnections().size());
+        manager.disconnect();
+        manager.disconnect("backup");
+        assertEquals(0, manager.getConnections().size());
+    }
+}
