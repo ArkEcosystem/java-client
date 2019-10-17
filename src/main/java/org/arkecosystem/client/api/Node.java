5815b6e3d3d27221c0eb0f42281898801a1f3c03
zan <zan@ark.io>
2019-10-17 12:45:41 +0200

    fix: fees endpoint

--- src/main/java/org/arkecosystem/client/api/Node.java
+++ src/main/java/org/arkecosystem/client/api/Node.java
@@ -29,10 +29,8 @@ public class Node {
         return this.client.get("node/configuration/crypto");
     }
 
-    public LinkedTreeMap<String, Object> fees(Integer... days) throws IOException {
-        HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("days", days.length > 0 ? days[0] : null);
-        return this.client.get("node/fees", parameters);
+    public LinkedTreeMap<String, Object> fees() throws IOException {
+        return this.client.get("node/fees");
     }
 
     public LinkedTreeMap<String, Object> debug() throws IOException {
