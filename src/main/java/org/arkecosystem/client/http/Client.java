c476ac4ff88256bc161bd36bfde146a9994dc0bf
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2020-11-11 13:39:38 -0300

    code cleanup

--- src/main/java/org/arkecosystem/client/http/Client.java
+++ src/main/java/org/arkecosystem/client/http/Client.java
@@ -1,29 +1,29 @@
 package org.arkecosystem.client.http;
 
 import com.google.gson.Gson;
-import com.google.gson.internal.LinkedTreeMap;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import okhttp3.*;
 
 public class Client {
+    private static final MediaType JSON = MediaType.parse("application/json");
+
     private final String host;
     private OkHttpClient client;
     private final Headers headers;
-    private final MediaType JSON = MediaType.parse("application/json");
 
     public Client(String host) {
         this.host = host;
         this.client = new OkHttpClient();
 
         HashMap<String, String> headers = new HashMap<>();
-        headers.put("content-type", this.JSON.toString());
+        headers.put("content-type", JSON.toString());
 
         this.headers = Headers.of(headers);
     }
 
-    public LinkedTreeMap<String, Object> get(String url, Map<String, Object> params)
+    public Map<String, Object> get(String url, Map<String, Object> params)
             throws IOException {
         HttpUrl.Builder httpBuilder = HttpUrl.parse(this.host + url).newBuilder();
 
@@ -38,19 +38,19 @@ public class Client {
 
         Response response = client.newCall(request).execute();
         return new Gson()
-                .fromJson(response.body().string(), new LinkedTreeMap<String, Object>().getClass());
+                .fromJson(response.body().string(), Map.class);
     }
 
-    public LinkedTreeMap<String, Object> get(String url) throws IOException {
+    public Map<String, Object> get(String url) throws IOException {
         return get(url, new HashMap<>());
     }
 
-    public LinkedTreeMap<String, Object> post(String url, Map<String, Object> payload) throws IOException {
+    public Map<String, Object> post(String url, Map<String, Object> payload) throws IOException {
         RequestBody body = RequestBody.create(JSON, new Gson().toJson(payload));
         Request request = new Request.Builder().url(this.host + url).post(body).build();
         Response response = client.newCall(request).execute();
         return new Gson()
-                .fromJson(response.body().string(), new LinkedTreeMap<String, Object>().getClass());
+                .fromJson(response.body().string(), Map.class);
     }
 
     public OkHttpClient getClient() {
