be7c60be35601a8d9fc6241ff752a5ce2cacbd56
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2020-11-12 11:03:37 -0300

    Delegates using params

--- src/main/java/org/arkecosystem/client/api/Delegates.java
+++ src/main/java/org/arkecosystem/client/api/Delegates.java
@@ -1,18 +1,27 @@
 package org.arkecosystem.client.api;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import org.arkecosystem.client.http.Client;
 
-public class Delegates {
+public class Delegates implements SupportsParams<Delegates> {
     private final Client client;
+
+    private final Map<String, Object> params = new LinkedHashMap<>();
 
     public Delegates(Client client) {
         this.client = client;
     }
 
+    @Override
+    public Delegates param(String name, Object value) {
+        params.put(name, value);
+        return this;
+    }
+
     public Map<String, Object> all() throws IOException {
-        return this.client.get("delegates");
+        return this.client.get("delegates", params);
     }
 
     public Map<String, Object> show(String id) throws IOException {
@@ -20,11 +29,15 @@ public class Delegates {
     }
 
     public Map<String, Object> blocks(String id) throws IOException {
-        return this.client.get("delegates/" + id + "/blocks");
+        return this.client.get("delegates/" + id + "/blocks", params);
     }
 
     public Map<String, Object> voters(String id) throws IOException {
-        return this.client.get("delegates/" + id + "/voters");
+        return this.client.get("delegates/" + id + "/voters", params);
+    }
+
+    public Map<String, Object> search() throws IOException {
+        return this.client.post("delegates/search", params);
     }
 
     public Map<String, Object> search(Map<String, Object> parameters) throws IOException {
