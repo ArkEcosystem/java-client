fbbfce97f82b424f6951b309b32912e395f32022
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-09-07 09:34:37 -0300

    wip

--- src/integration/java/api/TransactionsTest.java
+++ src/integration/java/api/TransactionsTest.java
@@ -1,7 +1,9 @@
-package integration;
+package api;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import org.arkecosystem.client.Connection;
+import org.arkecosystem.client.ConnectionManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,14 +11,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.arkecosystem.client.Connection;
-import org.arkecosystem.client.ConnectionManager;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.experimental.categories.Category;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-@Category(IntegrationTest.class)
 public class TransactionsTest {
 
     private Connection connection;
@@ -35,57 +32,57 @@ public class TransactionsTest {
     @Test
     void all() throws IOException {
         Map<String, Object> actual = connection.api().transactions.all();
-        MatcherAssert.assertThat(actual, Matchers.hasKey("meta"));
-        MatcherAssert.assertThat((Map<String, Object>) actual.get("meta"), Matchers.hasEntry("count", 100.0));
+        assertThat(actual, hasKey("meta"));
+        assertThat((Map<String, Object>) actual.get("meta"), hasEntry("count", 100.0));
     }
 
     @Test
     void create() throws IOException {
         Map<String, Object> actual = connection.api().transactions.create(new ArrayList<>());
-        MatcherAssert.assertThat(actual, Matchers.hasEntry("statusCode", 422.0));
+        assertThat(actual, hasEntry("statusCode", 422.0));
     }
 
     @Test
     void show() throws IOException {
         Map<String, Object> actual = connection.api().transactions.show("25b3a02400893e1bc5dc800368287ca9a120ad12b793eda9ba7ec3f36cd8fb3a");
-        MatcherAssert.assertThat(actual, Matchers.hasKey("data"));
-        MatcherAssert.assertThat((Map<String, Object>) actual.get("data"), Matchers.hasEntry("id", "25b3a02400893e1bc5dc800368287ca9a120ad12b793eda9ba7ec3f36cd8fb3a"));
+        assertThat(actual, hasKey("data"));
+        assertThat((Map<String, Object>) actual.get("data"), hasEntry("id", "25b3a02400893e1bc5dc800368287ca9a120ad12b793eda9ba7ec3f36cd8fb3a"));
     }
 
     @Test
     void allUnconfirmed() throws IOException {
         Map<String, Object> actual = connection.api().transactions.allUnconfirmed();
-        MatcherAssert.assertThat(actual, Matchers.hasKey("meta"));
+        assertThat(actual, hasKey("meta"));
         if (!((Map<String, Object>) actual.get("meta")).get("count").equals(0.0)) {
             List<Map<String, Object>> data = (List<Map<String, Object>>) actual.get("data");
             String id = (String) data.get(0).get("id");
             Map<String, Object> unconfirmed = connection.api().transactions.showUnconfirmed(id);
-            MatcherAssert.assertThat(unconfirmed, CoreMatchers.notNullValue());
-            MatcherAssert.assertThat(unconfirmed, CoreMatchers.instanceOf(Map.class));
+            assertThat(unconfirmed, notNullValue());
+            assertThat(unconfirmed, instanceOf(Map.class));
         }
     }
 
     @Test
     void showUnconfirmed() throws IOException {
         Map<String, Object> actual = connection.api().transactions.showUnconfirmed("25b3a02400893e1bc5dc800368287ca9a120ad12b793eda9ba7ec3f36cd8fb3a");
-        MatcherAssert.assertThat(actual, Matchers.hasEntry("statusCode", 404.0));
+        assertThat(actual, hasEntry("statusCode", 404.0));
     }
 
     @Test
     void types() throws IOException {
         Map<String, Object> actual = connection.api().transactions.types();
-        MatcherAssert.assertThat(actual, Matchers.hasKey("data"));
+        assertThat(actual, hasKey("data"));
     }
 
     @Test
     void schemas() throws IOException {
         Map<String, Object> actual = connection.api().transactions.schemas();
-        MatcherAssert.assertThat(actual, Matchers.hasKey("data"));
+        assertThat(actual, hasKey("data"));
     }
 
     @Test
     void fees() throws IOException {
         Map<String, Object> actual = connection.api().transactions.fees();
-        MatcherAssert.assertThat(actual, Matchers.hasKey("data"));
+        assertThat(actual, hasKey("data"));
     }
 }
