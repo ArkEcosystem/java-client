174ec0515886c2ec4758165b2b8f74071a6b224f
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-09-07 20:01:38 -0300

    Locks api tests

--- src/test/java/org/arkecosystem/client/api/LocksTest.java
+++ src/test/java/org/arkecosystem/client/api/LocksTest.java
@@ -1,13 +1,14 @@
 package org.arkecosystem.client.api;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import org.arkecosystem.client.Connection;
 import org.arkecosystem.client.MockHelper;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LocksTest {
 
@@ -36,14 +37,14 @@ public class LocksTest {
     @Test
     void searchUnlocked() throws IOException {
         Connection connection = MockHelper.connection();
-        Map<String, Object> actual = connection.api().locks.searchUnlocked(new HashMap<>());
+        Map<String, Object> actual = connection.api().locks.searchUnlocked();
         assertTrue((boolean) actual.get("success"));
     }
 
     @Test
     void searchUnlockedWithParams() throws IOException {
         Connection connection = MockHelper.connection();
-        Map<String, Object> actual = connection.api().locks.param("page", 1).searchUnlocked();
+        Map<String, Object> actual = connection.api().locks.searchUnlocked(Collections.singletonMap("page", 1));
         assertTrue((boolean) actual.get("success"));
     }
 }
