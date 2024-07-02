4c48a38ab60d77885a1f817aeb01ef40460fe1fe
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-07-02 11:29:02 -0600

    add new rounds methods

--- src/integration/java/org/arkecosystem/client/api/RoundsTest.java
+++ src/integration/java/org/arkecosystem/client/api/RoundsTest.java
@@ -7,13 +7,36 @@ import java.io.IOException;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasKey;
 
-public class RoundsTest extends BaseClientTest {
+public class RoundsIntegrationTest extends BaseClientTest {
 
     @Test
     void delegates() throws IOException {
         Map<String, Object> actual = connection.api().rounds.delegates(12345);
         assertThat(actual, hasKey("data"));
+    }
+
+    @Test
+    void all() throws IOException {
+        Map<String, Object> actual = connection.api().rounds.all();
+        assertThat(actual, hasKey("data"));
+        assertThat(actual, hasKey("meta"));
+    }
+
+    @Test
+    void allWithParams() throws IOException {
+        Map<String, Object> actual =
+                connection.api().rounds.param("page", 1).param("limit", 100).all();
+        assertThat(actual, hasKey("data"));
+        assertThat(actual, hasKey("meta"));
+    }
+
+    @Test
+    void show() throws IOException {
+        Map<String, Object> actual = connection.api().rounds.show(12345);
+        assertThat(actual, hasKey("data"));
+        assertThat((Map<String, ?>) actual.get("data"), hasEntry("id", 12345.0));
     }
 }
--- src/main/java/org/arkecosystem/client/api/Rounds.java
+++ src/main/java/org/arkecosystem/client/api/Rounds.java
@@ -1,17 +1,32 @@
 package org.arkecosystem.client.api;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import org.arkecosystem.client.http.Client;
 
 public class Rounds {
     private final Client client;
+    private final Map<String, Object> params = new LinkedHashMap<>();
 
     public Rounds(Client client) {
         this.client = client;
     }
 
+    public Rounds param(String name, Object value) {
+        params.put(name, value);
+        return this;
+    }
+
     public Map<String, Object> delegates(int id) throws IOException {
         return this.client.get("rounds/" + id + "/delegates");
+    }
+
+    public Map<String, Object> all() throws IOException {
+        return this.client.get("rounds", params);
+    }
+
+    public Map<String, Object> show(int roundId) throws IOException {
+        return this.client.get("rounds/" + roundId);
     }
 }
